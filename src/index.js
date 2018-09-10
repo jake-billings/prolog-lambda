6d6359e0333b054d98a226ede475e449b766a108
jakebillings <jake@jakebillings.com>
2018-09-10 19:00:28 -0400

    Add: index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/* Copyright (C) Jake Billings 2018
+ * Use granted under MIT LICENSE found in
+ * file in root directory of this repository
+ */
+export async function handler(body) {
+    //todo invoke prolog
+}
